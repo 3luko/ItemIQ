e09ba1e3e6981fe1d0f5ff777b7a325af0073e5d
Ethan <ethanlukandwa@gmail.com>
2024-03-09 04:02:34 -0600

    Created a slot adding feature to add slots for intel on what the user wanted to add. Also Created a feature that clears the slots.

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 
 const firstNameText = document.getElementById("firstNameText");
 const lastNameText = document.getElementById("lastNameText");
-const firstLastButton = document.querySelector("firstLastButton");
+var i = 1;
+
+
 
 
 
 function dispName(){
     firstNameText.innerHTML = document.getElementById("fName").value;
     lastNameText.innerHTML = document.getElementById("lName").value;
+}
+
+function clearDiv(){ //clears boxes
+    if(i > 1){
+        let div = document.getElementById("inventoryContainer");
+        div.replaceChildren();
+        alert("slots will be cleared");
+        i = 1;
+    } else {
+        alert("No slots have been created!");
+    }
+   
+}
+
+function createDiv(){ //creates boxes
+    newObj = document.createElement("div");
+    newObj.id = "slot" + i;
+    newObj.style.backgroundColor = "red";
+    newObj.style.vistibility = "show";
+    newObj.innerHTML = "Slot " + i;
+    document.getElementById("inventoryContainer").appendChild(newObj);
+    alert("Slot " + i + " will be added");
+    i++;
 }
