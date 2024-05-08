c9a32036963d6b61b102b563d18fac430283988b
Ethan <Ethanlukandwa@gmail.com>
2024-05-08 00:59:09 -0500

    just tester

--- tester.js
+++ tester.js
@@ -3,13 +3,13 @@ var num2 = 7;
 var num3 = 8;
 var num4 = 11; 
 var num5 = 13;
-
+/*
 const myArray = [num3, num1, num2, num5, num4];
 console.log("Array before the sort: " + myArray.join(" "));
 myArray.sort((a, b) => a - b);
 console.log("Array after the sort: " + myArray);
 //console.log(myArray.join(" "));
-
+*/
 
 function sortingAnArray(...arr){
     for(var i = 0; i < arr.length - 1; i++){
@@ -20,3 +20,26 @@ function sortingAnArray(...arr){
         }
     }
 }
+
+function fibonacciGenerator (n) {
+    //Do NOT change any of the code above 👆
+    var array = [];
+    for(var i = 0; i < n; i++){
+        if(i == 0){
+            array.push(0);
+        } else if(i == 1){
+            array.push(1);
+        } else {
+            var newNum = array[i - 2] + array[i - 1];
+            array.push(newNum);
+        }
+    }
+    
+    return array;
+        
+    //Do NOT change any of the code below 👇
+    }
+
+
+    var myArray = fibonacciGenerator(10);
+    console.log(myArray);
